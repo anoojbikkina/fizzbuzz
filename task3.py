241682b2850177deacb79bd5d497e0d05eb6f54d
Anooj Bikkina <anoojbikkina@TCBDD.WPA.Dal.Ca>
2019-05-23 11:51:39 -0300

    updated

--- task3.py
+++ task3.py
@@ -1,1 +1,20 @@
+def book_play():
 
+fread = open('book1.txt','r')
+fread = open('book2.txt','r')
+fread = open('book3.txt','r')
+
+fread = open('openbookfinal-2019-anoojbikkina/Book1.txt')
+fw1 = open('bookXunique.list','a+')
+fw2 = open('rarewords.list','a+')
+
+for i in fr1.readlines():
+	dict_check1.append(i.split(""))
+
+for i in fr2.readlines():
+	dict_check2.append(i.split(""))
+
+for i in fr3.readlines():
+	dict_check3.append(i.split(""))
+
+        retur()
