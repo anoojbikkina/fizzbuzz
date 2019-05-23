0bd4d36e01f335bf6b816b084dd420f6823f7ca0
Anooj Bikkina <anoojbikkina@TCBDD.WPA.Dal.Ca>
2019-05-23 10:50:27 -0300

    updated

--- task1.py
+++ task1.py
@@ -1,7 +1,15 @@
-
-# source - https://www.sanfoundry.com/python-program-copy-contents-file-another/#
-
-with open("test.txt") as f:
-    with open("out.txt", "w") as f1:
-        for line in f:
-            f1.write(line)
+def input_page(fr):
+        fw=open("test.txt","w")
+        count=0
+        pagestart= input("enter the starting page number")
+        pagend=input("enter the last page number")
+        for word in range(0,(25*pages),+1):
+        count+=1
+        if count>=(25*pages) and count<=(25*pageend):
+                for j in i:
+                        if j=='o' or j=='0':
+                                j='0'
+                        elif j=='e' or j=='E':
+                                j='3'
+                        elif j=='i' or j=='I'
+                                j='1'
