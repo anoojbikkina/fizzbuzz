889294156b6e5aa6bdac7af3d52651e6b1e5b4d6
Anooj Bikkina <anoojbikkina@TCBDD.WPA.Dal.Ca>
2019-05-23 11:46:54 -0300

    updated

--- task2.py
+++ task2.py
@@ -1,71 +1,39 @@
-os.listdir(path='.')
-
-Python
-'''
-For the given path, get the List of all files in the directory tree 
-'''
-def getListOfFiles(dirName):
-# create a list of file and sub directories 
-# names in the given directory 
-listOfFile = os.listdir(dirName)
-allFiles = list()
-# Iterate over all the entries
-for entry in listOfFile:
-# Create full path
-fullPath = os.path.join(dirName, entry)
-# If entry is a directory then get the list of files in this directory 
-if os.path.isdir(fullPath):
-allFiles = allFiles + getListOfFiles(fullPath)
-else:
-allFiles.append(fullPath)
-return allFiles
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-'''
-    For the given path, get the List of all files in the directory tree 
-'''
-def getListOfFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-                
-    return allFiles
-
-
-
 import os
+import hashlib
+
+filelist = list()
+
 def walk(dirname):
 	for name in os.listdir(dirname):
 		path = os.path.join(dirname,name)
 		if os.path.isfile(path):
-			print(path)
+			#print(path)
+			filelist.append(path)
 		else:
 			walk(path)
+
+def get_key(dict, value):
+	return [k for k,v in dict.items() if v==value]
+def checksum(filelist):
+	#md5 = hashlib.md5()
+	filedict = dict()
+	for filename in filelist:
+		md5 = hashlib.md5()
+		print(filename)
+		fr = open(filename,'rb')
+		for line in fr:
+			md5.update(line)
+		filedict[filename] = md5.hexdigest()
+		print("close file")
+		fr.close()
+	#print(filedict)
+	result = list()
+	for i in filedict.values():
+		keys = get_key(filedict,i)
+		if len(keys)>1:
+			result.append(keys)
+	print(result)
+
+walk("/home/student/final")#here should be a directory
+checksum(filelist)
 
